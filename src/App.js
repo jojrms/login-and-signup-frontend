61e9ed43f665e737eda967ec8a75d3e46d59e4a8
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-19 16:49:46 -0400

    💣 CODIFICAÇÃO Aplicação da biblioteca de rotas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import Login from "./components/login/Login";
 
 import './components/General.css'
 import { AuthProvider } from "./context/Auth";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./components/signup/SignUp";
 
 function App() {
   return (
-    
+
     <AuthProvider>
-      <div className="App">
-        <Login/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Login/>}/>  
+            <Route path='/create-accout' element={<SignUp/>}/>  
+          </Routes>
+        </div>
+      </BrowserRouter>
+      
     </AuthProvider>
     
   );
