e83278ba92d8497044d89f76f86865cfa6e18bff
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-10-19 10:39:47 -0400

    💣 CODIFICAÇÃO Finalização da estilização para mobile

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,4 +1,5 @@
 import './Login.css'
+import './LoginMobile.css'
 
 export default function Login(){
 
@@ -34,7 +35,7 @@ export default function Login(){
             </aside>
 
 
-            <aside className='asideBackgroundDecoration'>
+            <aside className='asideBackgroundDecoration' id='asideBackgroundDecoration'>
 
                 <article className='articleExbHouseDec'>
 
